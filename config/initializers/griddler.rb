ba3c3166cc0881777147a26b3d3848da44475646
Thomas PUJOL <thomaspujol69@gmail.com>
2024-10-24 23:28:33 +0200

    Above + FR

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -3,6 +3,6 @@ require 'email_processor'
 Griddler.configure do |config|
   config.processor_class = EmailProcessor # CommentViaEmail
   config.processor_method = :process # :create_comment (A method on CommentViaEmail)
-  config.reply_delimiter = '--- Make sure your reply appears above this line ---'
+  config.reply_delimiter = '--- Merci de bien vouloir répondre ci-dessus ---'
   config.email_service = Settings.mail_service # :sendgrid :cloudmailin, :postmark, :mandrill, :mailgun
 end
